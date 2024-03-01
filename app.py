a14891aa2c1fd7796462682f2d379e857f08b73c
AsgothXentio <152388913+AsgothXentio@users.noreply.github.com>
2024-03-01 12:10:15 +0100

    prepared the app.py, before adjusting html etc.

--- app.py
+++ app.py
@@ -1,1 +1,73 @@
-from flask import url_for, request, redirect, render_template
+from flask import (url_for, request, 
+                   redirect, render_template)
+from models import db, Project, app
+
+
+@app.route('/')
+def index():
+    project= Project.query.all()
+    return render_template('index.html', project=project)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_project():
+    if request.form:
+        new_project = Project(title=request.form['title'], created=request.form['created'], 
+                      description=request.form['description'], skill=request.form['skill'],
+                      url=request.form['url'], url_tag=request.form['alt']
+                      ,)
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    project = Project.query.get_or_404(id)
+
+    if project is None:
+
+        # Handle the case where the pet with the given id is not found
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        # Update the pet information only when the form is submitted
+        project.title = request.form['title']
+        project.created = request.form['created']
+        project.description = request.form['description']
+        project.skill = request.form['skill']
+        project.url = request.form['url']
+        project.url_tag = request.form['alt']
+
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    # Render the editpet.html template for the initial GET request
+    return render_template('edit.html', project=project)
+
+
+
+
+@app.route('/project/<id>')
+def project(id):
+    project = Project.query.get_or_404(id)
+    return render_template('projectform.html', project=project)
+
+
+@app.route('/delete/<id>')
+def delete_project(id):
+    project=Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8080, host='127.0.0.1')
--- models.py
+++ models.py
@@ -4,37 +4,25 @@ import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///product.db'
 db = SQLAlchemy(app)
 
 
-class Pet(db.Model):
+class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column('Title',db.String())
     created = db.Column('Created', db.DateTime, default=datetime.datetime.now)
-    name = db.Column('Name',db.String())
-    age = db.Column('Age',db.String())
-    breed = db.Column('Breed',db.String())
-    color = db.Column('Color',db.String())
-    size = db.Column('Size',db.String())
-    weight = db.Column('Weight',db.String())
+    description = db.Column('Description',db.Text())
+    skill = db.Column('Skills Practiced',db.String())
     url = db.Column('URL',db.String())
     url_tag = db.Column('Alt tag',db.String())
-    pet_type = db.Column('Pet type',db.String())
-    gender = db.Column('Gender',db.String())
-    spay = db.Column('Spay',db.String())
-    house_trained = db.Column('House Trained',db.String())
-    description = db.Column('Description',db.Text())
+
 
     def __repr__(self):
-        return f'''<Pet (Name: {self.name}
-        Age: {self.age}
-        Breed: {self.breed}
-        Color: {self.color}
-        Size: {self.size}
-        Weight: {self.weight}
+        return f'''<Project (Title: {self.title}
+        Created: {self.created}
+        Description: {self.description}
+        Skill: {self.skill}
         URL: {self.url}
-        Tag: {self.url_tag}
-        Gender: {self.gender}
-        Spay: {self.spay}
-        House Trained: {self.house_trained}
-        Desciprtion: {self.description}'''
+        Alt tag: {self.url_tag}
+        '''
